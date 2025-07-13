e55fbb581f9dc5cd76a5a80aa1d39e504b4810a7
Žikica Drača <zikica.draca@gmail.com>
2025-07-13 17:09:21 +0200

    header changes,lazy loading

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,32 @@
 import './header.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, lazy, Suspense } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import useIsMobile from '../../api/hooks/useIsMobile';
+import NavigateDesktop from './Navigate';
+const NavigateMobile = lazy(() => import('./Navigate'));
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [NavComponent, setNavComponent] = useState(null);
+  // const [NavComponent, setNavComponent] = useState(null);
   const location = useLocation();
   const isMobile = useIsMobile();
 
   const isHome = location.pathname === '/';
 
-  useEffect(() => {
-    if (!NavComponent) {
-      import('./Navigate')
-        .then((module) => {
-          const LoadedComponent = module.default;
-          if (typeof LoadedComponent === 'function') {
-            setNavComponent(() => LoadedComponent);
-          } else {
-            console.error('Loaded component is not a function:', module);
-          }
-        })
-        .catch(console.error);
-    }
-  }, [NavComponent]);
+  // useEffect(() => {
+  //   if (!NavComponent) {
+  //     import('./Navigate')
+  //       .then((module) => {
+  //         const LoadedComponent = module.default;
+  //         if (typeof LoadedComponent === 'function') {
+  //           setNavComponent(() => LoadedComponent);
+  //         } else {
+  //           console.error('Loaded component is not a function:', module);
+  //         }
+  //       })
+  //       .catch(console.error);
+  //   }
+  // }, [NavComponent]);
 
   const toggleMenu = () => {
     setIsOpen((prev) => !prev);
@@ -48,8 +50,16 @@ const Header = () => {
         <img src='/pictures/header/Logo-header.png' alt='logo' />
       </Link>
 
-      {NavComponent && (!isMobile || (isMobile && isOpen)) && (
+      {/* {NavComponent && (!isMobile || (isMobile && isOpen)) && (
         <NavComponent isOpen={isOpen} />
+      )} */}
+
+      {isMobile ? (
+        <Suspense fallback={null}>
+          {isOpen && <NavigateMobile isOpen={isOpen} />}
+        </Suspense>
+      ) : (
+        <NavigateDesktop isOpen={isOpen} />
       )}
 
       <button
